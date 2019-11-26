4b1c75eb9d9848d716578eebfe3eae5837336449
Olga Udalova <olga.udalova@kohls.com>
2019-11-26 23:34:02 +0300

    Clicks per seconds added; Autoclicker is an entity now; refactoring

--- src/clicker.js
+++ src/clicker.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './index.css';
+import autoclickImg from './autoclicker.png';
 
-// function Square (props) {
-//     return (
-//         <button className="square" onClick= {props.onClick}>
-//             {props.value}
-//         </button>
-//     );
-// }
+function Autoclicker (props) {
+    return (
+        <div>
+        <img src={autoclickImg} alt="autoclicker">
+
+        </img>
+        </div>
+    );
+}
 
 class Board extends React.Component {
     constructor(props){
@@ -16,10 +19,35 @@ class Board extends React.Component {
              cookieNum : 0,
              cookiePerClick : 1,
              cookiePerSecond : 0,
-             autoclickers: 0,
+             autoclickers: [],
              autoclickerPrice: 10
          }
     }
+
+    // renderArticles(articles) {
+    //     if (articles.length > 0) {
+    //         return articles.map((article, index) => (
+    //             <Article key={index} article={article} />
+    //         ));
+    //     }
+    //     else return [];
+    // }
+
+    componentDidMount() {
+        setInterval(() => this.setState({ cookieNum: this.state.cookieNum + this.state.cookiePerSecond}), 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    renderAutoclicker(i) {
+        return <Autoclicker
+            value={this.state.autoclickers[i]}
+            //onClick={() => this.handleClick(i)}
+        />
+    }
+
     increaseCookie() {
         let cookieNum = this.state.cookieNum;
         cookieNum += this.state.cookiePerClick;
@@ -31,16 +59,24 @@ class Board extends React.Component {
             alert("Not enough cookies!");
             return;
         }
-        this.setState({cookieNum : this.state.cookieNum - this.state.autoclickerPrice});
-        this.setState({autoclickers: this.state.autoclickers + 1}); //do I need this?
-        this.setState({cookiePerSecond : this.state.cookiePerSecond + this.state.autoclickers * 5});
-        this.setState({autoclikerPrice : this.state.autoclickerPrice += this.state.autoclickers * 20})
+        this.state.autoclickers.push()
+        let newCookieValue = this.state.cookieNum - this.state.autoclickerPrice;
+        let newAutoclickerValue = this.state.autoclickers.length * 5;
+        let newCookiesPerSecondValue = this.state.cookiePerSecond + newAutoclickerValue;
+        let newAutoclickerPrice = this.state.autoclickerPrice + this.state.autoclickers.length * 20
+        this.setState({cookieNum : newCookieValue});
+        this.setState({autoclickers: this.state.autoclickers.push(newAutoclickerValue)});
+        this.setState({cookiePerSecond : newCookiesPerSecondValue});
+        this.setState({autoclikerPrice : newAutoclickerPrice});
+        //return Autoclicker({value=)};
+        this.renderAutoclicker(this.state.autoclickers.length - 1)
     }
 
     render() {
         return (
              <div>
                  <div className="cookies">Cookies num: {this.state.cookieNum}</div>
+                 <div className="cookies">autocl: {this.state.autoclickers.length}</div>
                  <div className="cookies">Cookies per second: {this.state.cookiePerSecond}</div>
                  <button className="click" onClick= {() => this.increaseCookie()}>
                      Click!
